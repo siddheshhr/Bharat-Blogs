f945884ccca82ff506194fcd623a8911e93d8a84
siddheshhr <siddhesh.raskar04@gmail.com>
2024-06-23 16:12:16 +0530

    create signup api

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,4 @@
+const test=(req,res)=>{
+    res.json({message:'api working'});
+};
+export default test;
--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 // 
 mongoose.connect(process.env.MONGO)
@@ -13,9 +14,14 @@ mongoose.connect(process.env.MONGO)
   });
 
 const app = express();
+app.use(express.json());
 const PORT = 3000;
 
+app.use('/api/user', userRoutes);
+app.use('/api/auth',authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+
