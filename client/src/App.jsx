72f47390978a59a753fe312eecf5d4718266500b
siddheshhr <siddhesh.raskar04@gmail.com>
2024-06-25 23:41:51 +0530

    signinpage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Dashboard from "./Pages/Dashboard";
 import Signin from './Pages/Signin';
 import Projects from './Pages/Projects';
 import Header from './components/Header';
-
+import Footer from './components/Footer';
 export default function App() {
   return (
     <BrowserRouter>
@@ -19,6 +19,7 @@ export default function App() {
         <Route path='/dashboard' element={<Dashboard/>}/>
         <Route path='/project' element={<Projects/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   )
 }
