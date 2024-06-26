8a06fee6a6d3a1d5028f931bcc3354aac04d57dd
siddheshhr <siddhesh.raskar04@gmail.com>
2024-06-26 23:26:10 +0530

    add redux toolkit

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -1,12 +1,14 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch,useSelector } from "react-redux";
+import { signInStart,signInSuccess,signInFailure ,} from "../redux/user/userSlice";
 
 const SignIn = () => {
   const navigate = useNavigate();
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const {loading,error:errorMessage}=useSelector(state=>state.user);
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -15,27 +17,26 @@ const SignIn = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields");
+      return dispatch(signInFailure("Please fill out all fields"));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      setLoading(false);
+      if(data.success===false){
+        dispatch(signInFailure(data.message));
+      }
 
       if (res.ok) {
+        dispatch(signInSuccess(data));
         navigate('/');
-      } else {
-        setErrorMessage(data.message);
-      }
+      } 
     } catch (err) {
-      setErrorMessage("An error occurred. Please try again.");
-      setLoading(false);
+       dispatch(signInFailure(err.message));
     }
   };
 
