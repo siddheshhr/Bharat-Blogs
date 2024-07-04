b5dfc0e40e97926c15c76992f7879fb407978758
siddheshhr <siddhesh.raskar04@gmail.com>
2024-07-04 22:17:16 +0530

    add sidebar posts

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {useLocation} from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPost from '../components/DashPost';
 
 const Dashboard=()=>{
     const location = useLocation();
@@ -20,7 +21,9 @@ const Dashboard=()=>{
             <DashSidebar/>
         </div >
             {tab==='profile' && <DashProfile/>}
+            {tab=='posts' && <DashPost/>}
         </div>
+        
     )
 }
 export default Dashboard;
---
+++ client/src/components/DashPost.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const DashPost=()=> {
+  return (
+    <div>DashPost</div>
+  )
+}
+export default DashPost;
