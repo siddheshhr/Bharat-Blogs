8e0f61fad71ea4f1d8333e6bda00ded18ecfc25d
siddheshhr <siddhesh.raskar04@gmail.com>
2024-07-04 20:21:35 +0530

    comple post image

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.route.js';
+import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 // 
@@ -21,6 +22,7 @@ const PORT = 3000;
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth',authRoutes);
+app.use('/api/post',postRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
