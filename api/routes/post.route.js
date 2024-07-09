4ed524d7992449cff4e9ac549cb426e6b3bab627
siddheshhr <siddhesh.raskar04@gmail.com>
2024-07-09 20:54:13 +0530

    update & delete post function

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import {verifyToken} from '../utils/verifyUser.js';
-import create from '../controllers/post.controller.js';
-import getposts from '../controllers/getposts.controller.js';
+import create, { updatepost } from '../controllers/post.controller.js';
+import getposts, { deletepost } from '../controllers/getposts.controller.js';
 const router = express.Router();
 
 router.post('/create',verifyToken,create);
 router.get('/getposts',getposts);
+router.delete('/deletepost/:postId/:userId', verifyToken, deletepost);
+router.put('/updatepost/:postId/:userId',verifyToken,updatepost)
+///api/post/updatepost/
+
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './Pages/CreatePost';
+import UpdatePost from './Pages/UpdatePost';
 export default function App() {
   return (
     <BrowserRouter>
@@ -24,6 +25,9 @@ export default function App() {
         </Route>
         <Route element={<OnlyAdminPrivateRoute/>}>
          <Route path='/create-post' element={<CreatePost/>}/>
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute/>}>
+         <Route path='/update-post/:postId' element={<UpdatePost/>}/>
         </Route>
         <Route path='/project' element={<Projects/>}/>
       </Routes>
