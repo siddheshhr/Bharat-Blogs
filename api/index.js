cb289bc7ef6c83dd09dfae41bace8c42af461464
siddheshhr <139670479+siddheshhr@users.noreply.github.com>
2024-10-24 16:04:22 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import authRoutes from './routes/auth.route.js';
 import postRoutes from './routes/post.route.js';
 import cookieParser from 'cookie-parser';
 import commentRoutes from './routes/comment.route.js';
+import path from 'path'
 
 dotenv.config();
 // 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.MONGO)
   .catch((error) => {
     console.error("Error Connecting to MongoDB:", error);
   });
+  const __dirname = path.resolve();
 
 const app = express();
 app.use(cookieParser());
@@ -27,6 +29,11 @@ app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
 app.use('/api/comment', commentRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
